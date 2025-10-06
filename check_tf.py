c9036c0951a8b5a5316c555882c8504ead600311
Paakallo <paviveerar@gmail.com>
2025-10-06 08:50:42 +0200

    training changes

--- check_tf.py
+++ check_tf.py
@@ -1,11 +1,11 @@
 import tensorflow as tf
 
-interpreter = tf.lite.Interpreter(model_path="neural_filter.tflite")
+interpreter = tf.lite.Interpreter(model_path="conv_downsize.tflite", experimental_delegates=[])
 interpreter.allocate_tensors()
 for tensor in interpreter.get_tensor_details():
-    print(tensor['name'], '\n')
+    print(tensor['name'])
 
 print('Printing ops \n')
 
 for op_detail in interpreter._get_ops_details():
-    print(op_detail['op_name'], '\n')
+    print(op_detail['op_name'])
