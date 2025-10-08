ad41137726202f2dbc5eb5dc46dc78c3177a9c3b
Paakallo <paviveerar@gmail.com>
2025-10-08 11:23:10 +0200

    try adding sequences

--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 import numpy as np
+
+
 def generate_data(num_samples=1000, noise_factor=0.5):
     np.random.seed(42)
     t = np.linspace(0, 1, num_samples, dtype=np.float32)
@@ -9,3 +11,12 @@ def generate_data(num_samples=1000, noise_factor=0.5):
     s.astype(dtype=np.float32)
     return x, s
 
+
+def create_windows(x, s, window_size=10):
+    X, Y = [], []
+    for i in range(window_size, len(x)):
+        X.append(x[i-window_size:i])  # 10 samples (previous 9 + current)
+        Y.append(s[i])                # clean target for the current sample
+    X = np.array(X)[..., np.newaxis]  # shape: (samples, 10, 1)
+    Y = np.array(Y)[..., np.newaxis]  # shape: (samples, 1)
+    return X.astype(np.float32), Y.astype(np.float32)
